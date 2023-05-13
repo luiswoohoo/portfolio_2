f033677b4fc46319289a8ad94d41e34062999f3e
Luis <luiswoohoo@gmail.com>
2023-05-12 19:19:30 -0500

    Early draft

---
+++ src/scripts/projectsData.js
@@ -0,0 +1,40 @@
+const projectsData = [
+    {
+        name: 'Card Stack',
+        tech: 'React • Styled Components',
+        description: `I was looking around for a simple flashcard app, but most of the apps that I found required signing up and they just didn't have the look and feel that I wanted. So I decided to make my own.`,
+        src: 'CardStack_gttvbn',
+        width: '700',
+        height: '350',
+        alt: 'Flashcards app',
+    },
+    {
+        name: 'Quizzical',
+        tech: 'React',
+        description: 'A quiz app built with React that pulls trivia from the Open Trivia DB API. The design is from Scrimba.com, but I coded the app from scratch.',
+        src: 'Quizzical_mgqb7k',
+        width: '700',
+        height: '350',
+        alt: 'Quiz app',
+    },
+    {
+        name: 'Movie Finder',
+        tech: 'JS • Serverless Functions',
+        description: 'A movie finder app built in vanilla JS that pulls movie data from the OMDB API. A couple of serverless functions are used to hide the API key from the client. The design is from Scrimba.com, but I built the app completely from scratch.',
+        src: 'MovieFinder_figuse',
+        width: '700',
+        height: '350',
+        alt: 'Movide finder app',
+    },
+    {
+        name: 'Google Apps Script',
+        tech: 'Google Apps Script',
+        description: `I had been creating invoices for a trucking business and wondered if there was a way to automate some of the more repetitive tasks. That's when I discovered Google Apps Script. It's essentially JS with methods that tap into Google Drive. I was able to speed up my workflow and discovered how much I enjoy coding. This is what sparked my motivation to look into coding as a career.`,
+        src: 'GAS_kyjfhj',
+        width: '700',
+        height: '350',
+        alt: 'Google Apps Script',
+    },
+]
+
+export { projectsData }
