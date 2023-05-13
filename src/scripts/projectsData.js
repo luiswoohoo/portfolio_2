96ce53e31aad2a802b074b76a224217aa21cac1e
Luis <luiswoohoo@gmail.com>
2023-05-13 09:29:15 -0500

    Added links to projects

--- src/scripts/projectsData.js
+++ src/scripts/projectsData.js
@@ -2,7 +2,7 @@ const projectsData = [
     {
         name: 'Card Stack',
         tech: 'React • Styled Components',
-        description: `I was looking around for a simple flashcard app, but most of the apps that I found required signing up and they just didn't have the look and feel that I wanted. So I decided to make my own.`,
+        description: `I was searching around for a simple flashcard app, but most of the apps that I found required signing up and they just didn't have the look and feel that I wanted. So I decided to make my own.`,
         src: 'CardStack_gttvbn',
         width: '700',
         height: '350',
@@ -11,7 +11,7 @@ const projectsData = [
     {
         name: 'Quizzical',
         tech: 'React',
-        description: 'A quiz app built with React that pulls trivia from the Open Trivia DB API. The design is from Scrimba.com, but I coded the app from scratch.',
+        description: `A quiz app built with React that pulls trivia from the Open Trivia DB API. The design is from Scrimba.com, but I coded the app from scratch.`,
         src: 'Quizzical_mgqb7k',
         width: '700',
         height: '350',
@@ -20,7 +20,7 @@ const projectsData = [
     {
         name: 'Movie Finder',
         tech: 'JS • Serverless Functions',
-        description: 'A movie finder app built in vanilla JS that pulls movie data from the OMDB API. A couple of serverless functions are used to hide the API key from the client. The design is from Scrimba.com, but I built the app completely from scratch.',
+        description: `A movie finder app built in vanilla JS that pulls movie data from the OMDB API. A couple of serverless functions are used to hide the API key from the client. The design is from Scrimba.com, but I built the app completely from scratch.`,
         src: 'MovieFinder_figuse',
         width: '700',
         height: '350',
@@ -29,7 +29,7 @@ const projectsData = [
     {
         name: 'Google Apps Script',
         tech: 'Google Apps Script',
-        description: `I was creating invoices for a trucking business and wondered if there was a way to automate some of the more repetitive tasks. That's when I discovered Google Apps Script. It's essentially JS with methods that tap into Google Drive. I was able to speed up my workflow and discovered how much I enjoy coding. This is what sparked my motivation to look into coding as a career.`,
+        description: `I was creating invoices for a trucking business and wondered if there was a way to automate the process. That's when I discovered Google Apps Script. It's essentially JS with methods that tap into Google Drive. I was able to speed up my workflow and discovered how much I enjoy coding. This sparked my motivation to consider coding as a career.`,
         src: 'GAS_kyjfhj',
         width: '700',
         height: '350',
