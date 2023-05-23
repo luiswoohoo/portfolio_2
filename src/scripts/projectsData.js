f9a7685451846492af7c39fcff09506956982f7e
Luis <luiswoohoo@gmail.com>
2023-05-23 11:26:21 -0500

    Layout changes, somewhat responsive

--- src/scripts/projectsData.js
+++ src/scripts/projectsData.js
@@ -4,8 +4,8 @@ const projectsData = [
         tech: 'React • Styled Components',
         description: `I was searching around for a simple flashcard app, but most of the apps that I found required signing up and they just didn't have the look and feel that I wanted. So I made my own.`,
         src: 'CardStack_gttvbn',
-        width: '700',
-        height: '350',
+        width: '500',
+        height: '480',
         alt: 'Flashcards app',
         longDescription: {
             intro: `I was searching around for a simple flashcard app, but most of the apps that I found required signing up and they just didn't have the look and feel that I wanted. So I decided to make my own. I wanted something minimalistic without a bunch of features getting in the way of the simple process of flipping a card and then moving on to the next one.`,
@@ -20,8 +20,8 @@ const projectsData = [
         tech: 'React',
         description: `A quiz app built with React that pulls trivia from the Open Trivia DB API. The design is from Scrimba.com, but I coded the app from scratch.`,
         src: 'Quizzical_mgqb7k',
-        width: '700',
-        height: '350',
+        width: '500',
+        height: '480',
         alt: 'Quiz app',
         longDescription: {
             intro: ``,
@@ -36,8 +36,8 @@ const projectsData = [
         tech: 'JS • Serverless Functions',
         description: `A movie app built in vanilla JS that pulls movie data from the OMDB API. A couple of serverless functions are used to hide the API key from the client. The design is from Scrimba.com, but I built the app completely from scratch.`,
         src: 'MovieFinder_figuse',
-        width: '700',
-        height: '350',
+        width: '500',
+        height: '480',
         alt: 'Movie finder app',
         longDescription: {
             intro: ``,
@@ -52,8 +52,8 @@ const projectsData = [
         tech: 'Google Apps Script',
         description: `I was creating invoices for a trucking company and wondered if there was a way to automate the process. That's when I discovered Google Apps Script. I was able to speed up my workflow and discovered how much I enjoy coding.`,
         src: 'GAS_kyjfhj',
-        width: '700',
-        height: '350',
+        width: '500',
+        height: '480',
         alt: 'Google Apps Script',
         longDescription: {
             intro: `I was creating invoices for a trucking company and wondered if there was a way to automate the process. That's when I discovered Google Apps Script. It's essentially JS with methods that tap into Google Drive. I was able to speed up my workflow and discovered how much I enjoy coding. This sparked my motivation to consider coding as a career.`,
