535947d65ba2d264bbe78b00f92637edfecd2d3d
Luis <luiswoohoo@gmail.com>
2023-06-02 17:05:45 -0500

    Contact form and Footer update

--- src/scripts/scrollToAnchor.js
+++ src/scripts/scrollToAnchor.js
@@ -1,6 +1,5 @@
-export function scrollToAnchor(selectedAnchor) {
+function scrollToAnchor(selectedAnchor) {
     if (!document.querySelector(selectedAnchor)) {
-        console.log('no selected anchor')
         window.location.href = `http://localhost:3000/${selectedAnchor}`
     } else {
         document.querySelector(selectedAnchor).scrollIntoView({
