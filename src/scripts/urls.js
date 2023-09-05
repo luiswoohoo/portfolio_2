918104ed8a84d04f5fd2aff6850e6f9bcbb8f847
Luis <luiswoohoo@gmail.com>
2023-09-04 21:44:10 -0500

    Added view transitions, updated dev localhost

--- src/scripts/urls.js
+++ src/scripts/urls.js
@@ -2,6 +2,6 @@ const prod = {
     url: 'https://luisaguinaga.dev/',
 }
 const dev = {
-    url: 'http://localhost:3000/',
+    url: 'http://localhost:4321/',
 }
 export const config = import.meta.env.MODE === 'development' ? dev : prod
